e9ac1ca0eed5b801cd681263b166ba9ffc551753
NANOR3N <NANO4REN@GMAIL.COM>
2022-09-14 20:36:38 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ejercicio18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un numero");
+            int unNumero = int.Parse(Console.ReadLine());
+
+            if (unNumero > 0) Console.WriteLine("El numero ingresado es POSITIVO");
+            else if (unNumero < 0) Console.WriteLine("El numero ingresado es NEGATIVO");
+            else Console.WriteLine("El numero ingresado es 0");
+            
+
+        }
+    }
+}
